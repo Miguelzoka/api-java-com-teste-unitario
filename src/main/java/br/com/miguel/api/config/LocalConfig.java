e74a49efeade6e72ee1c62464f963405395b4e18
Miguel Lima <miguel.pwg@gmail.com>
2026-06-18 11:18:52 -0300

    perfil de testes

---
+++ src/main/java/br/com/miguel/api/config/LocalConfig.java
@@ -0,0 +1,29 @@
+package br.com.miguel.api.config;
+
+import br.com.miguel.api.domain.User;
+import br.com.miguel.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Bean
+    public CommandLineRunner startDB() {
+        return args -> {
+            User u1 = new User(null, "Miguel", "miguel@mail.com", "123");
+            User u2 = new User(null, "Maria", "maria@mail.com", "123");
+
+            repository.saveAll(List.of(u1, u2));
+        };
+    }
+}
--- src/main/java/br/com/miguel/api/domain/User.java
+++ src/main/java/br/com/miguel/api/domain/User.java
@@ -9,6 +9,7 @@ import java.util.Objects;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
