ff179260e539f0d2727506c43e5598e33b914d7a
Miguel Lima <miguel.pwg@gmail.com>
2026-06-18 09:57:30 -0300

    user class

---
+++ src/main/java/br/com/miguel/api/domain/User.java
@@ -0,0 +1,23 @@
+package br.com.miguel.api.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
