95eaa97a36fd7de192de24ffa16c73ba17a092f8
Miguel Lima <miguel.pwg@gmail.com>
2026-06-19 10:10:24 -0300

    endpoint findAll para user

--- src/main/java/br/com/miguel/api/services/UserService.java
+++ src/main/java/br/com/miguel/api/services/UserService.java
@@ -2,7 +2,10 @@ package br.com.miguel.api.services;
 
 import br.com.miguel.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findByID(Integer id);
+    List<User> findAll();
 }
