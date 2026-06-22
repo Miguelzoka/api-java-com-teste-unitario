80f005ccea5f80f3674b45d77d70d638fed5f9c3
Miguel Lima <miguel.pwg@gmail.com>
2026-06-22 18:58:22 -0300

    excecao para email ja cadastrado

--- src/main/java/br/com/miguel/api/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/miguel/api/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.miguel.api.resources.exceptions;
 
+import br.com.miguel.api.services.exceptions.DataIntegratyViolationException;
 import br.com.miguel.api.services.exceptions.ObjectNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -16,5 +17,11 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError>objectNotFound(ObjectNotFoundException ex, HttpServletRequest request) {
         StandardError error = new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(DataIntegratyViolationException.class)
+    public ResponseEntity<StandardError>dataIntegratyViolationExcpetion(DataIntegratyViolationException ex, HttpServletRequest request) {
+        StandardError error = new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
